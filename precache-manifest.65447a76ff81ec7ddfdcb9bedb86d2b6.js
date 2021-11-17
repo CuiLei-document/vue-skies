c4f4bc7d286997f34c1ffa99f1d0806f4df03cb6
磊哥 <493114008@qq.com>
2021-11-17 22:18:26 +0800

    deploy

---
+++ precache-manifest.65447a76ff81ec7ddfdcb9bedb86d2b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "490934c80392f7fef628",
+    "url": "/vue-skies/css/app.fb235ad0.css"
+  },
+  {
+    "revision": "e23f638cda43cfd879b97704bf053c71",
+    "url": "/vue-skies/index.html"
+  },
+  {
+    "revision": "490934c80392f7fef628",
+    "url": "/vue-skies/js/app.c218f404.js"
+  },
+  {
+    "revision": "898c91fefa5d02358c0c",
+    "url": "/vue-skies/js/chunk-vendors.d2aa8153.js"
+  },
+  {
+    "revision": "4526bf98f16c9f9f6fa09fbc2e6e58f1",
+    "url": "/vue-skies/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-skies/robots.txt"
+  },
+  {
+    "revision": "96b3b46da57936f658ed36113ee3bddf",
+    "url": "/vue-skies/skies.png"
+  }
+]);
